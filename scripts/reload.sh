c3b3d67e58b32c2d22bb1135ac2c690dd4472ca0
root <root@debug010000002015.local.alipay.net>
2018-04-22 21:50:58 +0800

    hello

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dir=$(cd `dirname $0`; pwd)
+echo $LD_LIBRARY_PATH 
 export LD_LIBRARY_PATH=$dir/../luajit/lib:$dir/../nginx/urslib/:$LD_LIBRARY_PATH
 bin="$dir/../nginx/sbin/nginx"
 if $bin -t;then
